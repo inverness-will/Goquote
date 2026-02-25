e6e003560e33479ee2a6f4c793e044afb6245401
inverness-will <william.macdonald@gmail.com>
2026-02-25 13:44:38 +0000

    favicon

--- scripts/inject-favicon.js
+++ scripts/inject-favicon.js
@@ -4,7 +4,7 @@ const path = require('path');
 const indexPath = path.join(__dirname, '../dist/index.html');
 let html = fs.readFileSync(indexPath, 'utf8');
 
-const faviconTag = '    <link rel="icon" href="/favicon.ico" />';
+const faviconTag = '    <link rel="icon" href="favicon.ico" />';
 if (html.includes('rel="icon"')) {
   console.log('Favicon link already present in index.html');
 } else {
